3783dad2276eac672191dd79d38f5afaccb511b9
Ravend1028 <ravendavid.rd30@gmail.com>
2024-03-21 21:04:45 +0800

    added bg

--- index.php
+++ index.php
@@ -1,13 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>EngineerHub</title>
+<?php include 'inc/header.php'; ?>
 
-</head>
-<body>
+  <div class="container-fluid m-0 p-0">
+    <div class="row">
+      <div class="col-12 position-relative">
+
+        <video src="videos/Electronic Circuit video.mp4" class="object-fit-cover w-100 position-absolute top-0 start-0" autoplay loop></video>
+
+        <div class="position-absolute w-75 z-index-1" style="transform: translateY(40%);">
+          <h1 class="text-white">
+            EngineerHub: Your One Stop E-commerce Platform for Engineering Materials
+          </h1> 
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+<?php include 'inc/footer.php'; ?>
+
+
   
-</body>
-</html>
