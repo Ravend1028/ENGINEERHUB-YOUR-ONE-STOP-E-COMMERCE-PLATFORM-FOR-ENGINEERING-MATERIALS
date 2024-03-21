245927fdce8067058b38868401e7f6a2e5a94ae8
Ravend1028 <ravendavid.rd30@gmail.com>
2024-03-22 01:04:00 +0800

    align text in home section

--- index.php
+++ index.php
@@ -1,20 +1,21 @@
 <?php include 'inc/header.php'; ?>
 
-  <div class="container-fluid m-0 p-0">
-    <div class="row">
-      <div class="col-12 position-relative">
+<div class="container-fluid">
+    <video src="videos/Electronic Circuit video.mp4" class="object-fit-cover w-100 position-absolute top-0 start-0" autoplay loop></video>
+    
+    <div class="container position-absolute p-5 w-75 h-100 d-lg-flex flex-lg-column justify-content-lg-center align-items-lg-center">
+      <h1 class="text-white">
+        EngineerHub: Your One Stop E-commerce Platform for Engineering Materials
+      </h1>
 
-        <video src="videos/Electronic Circuit video.mp4" class="object-fit-cover w-100 position-absolute top-0 start-0" autoplay loop></video>
-
-        <div class="position-absolute w-75 z-index-1" style="transform: translateY(40%);">
-          <h1 class="text-white">
-            EngineerHub: Your One Stop E-commerce Platform for Engineering Materials
-          </h1> 
-        </div>
-
+      <div class="container m-0 pt-4">
+        <button type="button" class="btn btn-dark btn-lg p-3">Explore More</button>
       </div>
     </div>
-  </div>
+</div>
+
+
+
 
 <?php include 'inc/footer.php'; ?>
 
